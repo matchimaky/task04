b13812730a7ef6727cc114a9bfe75316e4422144
matchima <mkhamyod@gmail.com>
2024-08-08 21:33:43 +0700

    Q 5.3

--- src/router/index.ts
+++ src/router/index.ts
@@ -79,7 +79,8 @@ const routes = [
     path: '/:catchAll(.*)',
     name: 'not-found',
     component: NotFoundView
-  },
+  }
+  ,
   {
     path: '/network-error',
     name: 'network-error-view',
@@ -90,7 +91,17 @@ const routes = [
     name: 'student-list-view',
     component: StudentListView
   }
-]
+],
+
+// scrollBehavior( to,from,savedPosition){
+//   if (savedPosition){
+//     return savedPosition
+//   } else {
+//     return { top: 0}
+//   }
+// }
+
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
